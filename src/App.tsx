6760883a3053acb936cb926fed245a3fdcbf10c3
Luca Di Fazio <highliuk@gmail.com>
2022-08-04 21:42:20 +0200

    ✨ Stone Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { FC } from "react"
+import { Stone } from "./components/Stone"
 
 const App: FC = () => {
   return (
     <div className="flex h-screen items-center justify-center bg-page bg-cover p-8">
       <div className="flex aspect-square w-full max-w-[75vh] items-center justify-center rounded-md border border-gray-400 border-opacity-30 bg-gradient-to-tl from-orange-200 to-orange-100 shadow-xl sm:my-32">
-        <div className="aspect-square w-full bg-board bg-cover"></div>
+        <div className="flex aspect-square w-full items-center justify-center bg-board bg-cover">
+          <div className="relative aspect-square w-[88.88%]">
+            {/* <Stone color="black" x={1} y={0} /> */}
+          </div>
+        </div>
       </div>
     </div>
   )
---
+++ src/components/Stone/Stone.tsx
@@ -0,0 +1,30 @@
+import { FC } from "react"
+
+type Props = {
+  color: "black" | "white"
+  x: number
+  y: number
+}
+
+const Stone: FC<Props> = ({ color, x, y }) => {
+  return (
+    <div
+      className={cn([
+        "absolute aspect-square w-[10%] -translate-x-1/2 -translate-y-1/2 rounded-full bg-gradient-to-br shadow",
+        color === "black"
+          ? "from-gray-700 to-black"
+          : "from-gray-50 to-gray-300",
+      ])}
+      style={{
+        top: `${12.5 * y}%`,
+        left: `${12.5 * x}%`,
+      }}
+    ></div>
+  )
+}
+
+function cn(classes: string[]): string {
+  return classes.join(" ")
+}
+
+export default Stone
