94e0ab6370601422563627b6980b69f99080088a
Luca Di Fazio <highliuk@gmail.com>
2022-08-09 00:28:23 +0200

    🔧 CORS

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,13 @@
 import express, { Request, Response } from "express"
+import cors from "cors"
 import { config } from "dotenv"
 
 config()
 
 const app = express()
 const port = process.env.PORT
+
+app.use(cors({ origin: process.env.CLIENT_URL }))
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!")
