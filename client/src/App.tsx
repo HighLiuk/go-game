ca4d3923b6e1244f731afb51d9611ccf31482906
Luca Di Fazio <highliuk@gmail.com>
2022-08-09 00:21:10 +0200

    ♻ Board Component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,11 @@
 import { FC } from "react"
-import { Squares } from "./components/Squares"
+import { Board } from "./components/Board"
 
 const App: FC = () => {
   return (
-    <div className="flex h-screen items-center justify-center bg-page bg-cover p-8">
-      <div className="flex aspect-square w-full max-w-[75vh] items-center justify-center rounded-md border border-gray-400 border-opacity-30 bg-gradient-to-tl from-orange-200 to-orange-100 shadow-xl sm:my-32">
-        <div className="flex aspect-square w-full items-center justify-center bg-board bg-cover">
-          <div className="relative aspect-square w-[88.88%]">
-            <Squares />
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+      <Board />
+    </>
   )
 }
 
