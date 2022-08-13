b8213198524602c7ece8c78235b09119e405a07a
Luca Di Fazio <highliuk@gmail.com>
2022-08-13 16:06:57 +0200

    ✨ Create Match Endpoint

--- server/src/app.spec.ts
+++ server/src/app.spec.ts
@@ -1,14 +1,25 @@
+import { PrismaClient } from "@prisma/client"
 import request from "supertest"
 import app from "./app"
+
+const prisma = new PrismaClient()
 
 describe("Routes", () => {
   const client = request(app)
 
   describe("POST /matches", () => {
     it("should create a new match", async () => {
-      const res = await client.get("/").send()
+      const res = await client.post("/matches")
+      const { id } = res.body as { id: string }
 
-      expect(res.statusCode).toEqual(200)
+      expect(res.statusCode).toEqual(201)
+      expect(res.body).toHaveProperty("id")
+      expect(id).toHaveLength(36)
+
+      const match = await prisma.match.findFirst({ where: { id } })
+
+      expect(match).not.toBeNull()
+      expect(match?.id).toBe(id)
     })
   })
 })
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,17 +1,19 @@
 import express, { Request, Response } from "express"
 import cors from "cors"
 import { config } from "dotenv"
+import { PrismaClient } from "@prisma/client"
 
 config()
 
 const app = express()
+const prisma = new PrismaClient()
 
 app.use(cors({ origin: process.env.CLIENT_URL }))
 
-app.get("/", (req: Request, res: Response) => {
-  const matchId = "1"
+app.post("/matches", async (req: Request, res: Response) => {
+  const match = await prisma.match.create({ data: { size: 9 } })
 
-  res.json({ matchId })
+  res.status(201).json({ id: match.id })
 })
 
 export default app
